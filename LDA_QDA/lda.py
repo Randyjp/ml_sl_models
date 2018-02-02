0a2fdc1e2ae12ebb9f704cb4ded6b9c5de5edaf6
Randy <randy@Randys-MacBook-Pro.local>
2018-02-02 15:00:16 -0400

    scatter plot of all classes

--- LDA_QDA/lda.py
+++ LDA_QDA/lda.py
@@ -8,9 +8,11 @@
 
 
 from sklearn.datasets import load_wine
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis, QuadraticDiscriminantAnalysis
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.model_selection import cross_val_predict, train_test_split
 from sklearn.metrics import confusion_matrix, precision_score, recall_score, f1_score
+import matplotlib.pyplot as plt
+import pandas as pd
 
 wine = load_wine()
 X = wine.data
@@ -19,7 +21,7 @@ y = wine.target
 # fit the model
 lda = LinearDiscriminantAnalysis()
 lda_fit = lda.fit(X, y)
-print(lda_fit)
+# print(lda_fit)
 
 # compute accuracy metrics, keep in mind this is ONLY the training error. We might be over|under fitting
 
@@ -45,7 +47,7 @@ predicted_cv2 = cross_val_predict(lda_fit2, X_test, y_test, cv=10)
 
 # confusion matrix
 con_matrix2 = confusion_matrix(y_test, predicted_cv2)
-print(con_matrix2) # [[26  0  0][ 2 24  1] [ 0  0 19]
+# print(con_matrix2) # [[26  0  0][ 2 24  1] [ 0  0 19]
 
 
 # Precision, Recall and F metric
@@ -54,3 +56,16 @@ recall2 = recall_score(y_test, predicted_cv2, average='micro')  # 0.958333333333
 f1_2 = f1_score(y_test, predicted_cv2, average='micro')  # 0.9583333333333334
 
 
+# Plotting multi-dimensional data.
+# LDA can be used for dimensionality reduction and that's a good approach to visualize the
+# spread of classes in our data
+lda_reduce = LinearDiscriminantAnalysis(n_components=2)
+lda_transformed = pd.DataFrame(lda_reduce.fit_transform(X, y))
+
+# graph our 3 class
+plt.scatter(lda_transformed[y == 0][0], lda_transformed[y == 0][1], label='class 1', c='red')
+plt.scatter(lda_transformed[y == 1][0], lda_transformed[y == 1][1], label='class 2', c='blue')
+plt.scatter(lda_transformed[y == 2][0], lda_transformed[y == 2][1], label='class 3', c='yellow')
+
+plt.legend(loc=3)
+plt.show()
